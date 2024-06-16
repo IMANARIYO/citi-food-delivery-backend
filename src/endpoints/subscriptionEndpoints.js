0d06b108c852d84ab4370be5d5d1d2d55e62bd8f
IMANARIYO <imanariyobaptiste@gmail.com>
2024-06-16 21:43:16 +0200

    matching end points with swagger for subscrpton

--- src/endpoints/subscriptionEndpoints.js
+++ src/endpoints/subscriptionEndpoints.js
@@ -6,7 +6,7 @@ const subscriptionRouter = express.Router();
 
 subscriptionRouter.post('/create-subscription', createModelHandler(Subscription));
 subscriptionRouter.get('/get-all', readModelHandler(Subscription));
-subscriptionRouter.get('/get-subscription-by-id/:id', readModelHandler(Subscription));
+subscriptionRouter.get('/getby-id/:id', readModelHandler(Subscription));
 subscriptionRouter.put('/update/:id', updateModelHandler(Subscription));
 subscriptionRouter.delete('/delete/:id', deleteModelHandler(Subscription));
 
